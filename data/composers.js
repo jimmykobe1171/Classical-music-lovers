a719c85af667c8fea945f1847a773bcd4414a13d
annakoanna <anna.231991k@gmail.com>
2022-06-14 10:44:18 -0700

    audio is working

--- data/composers.js
+++ data/composers.js
@@ -4,7 +4,7 @@ const db = mongoose.connection
   db.on("open", () =>{
       const composersArr = [{
           name: "Wolfgang Amadeus Mozart",
-          songs: [{name:"Overture to the marriage of Figaro"  , audio: "Overture-to-The-marriage-of-Figaro-K.-492.mp3"}]
+          songs: [{name:"Overture to the marriage of Figaro"  , audio: "audio/Overture-to-The-marriage-of-Figaro-K.-492.mp3"}]
       },{name: "F.List" },{name: "Pyotr Ilyich Tchaikovsky"}, {name:"Frédéric Chopin"}, {name:"Claude Debussy"}, {name:"Ludwig van Beethoven"}, {name:"Robert Schumann"}, {name:"Camille Saint-Saëns "}, {name:"Igor Stravinsky "}, {name:"Sergei Rachmaninov"}
       
       ]
--- models/playlist.js
+++ models/playlist.js
@@ -5,10 +5,10 @@ const { Schema, model } = mongoose;
 const playlistSchema = new Schema({
   songs: [{ type: Schema.Types.ObjectId,
     ref: "Song"}],
-  user: {
+  user: [{
     type: Schema.Types.ObjectId,
     ref: "User"
-  }
+  }]
 });
 const Playlist = model("Playlist", playlistSchema);
 
